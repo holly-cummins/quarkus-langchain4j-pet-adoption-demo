4bd900b52327bab083ba0885eabbc9a6b9821efc
Holly Cummins <holly.k.cummins@googlemail.com>
2026-05-06 08:43:41 +0100

    Add a heading

--- src/main/webui/src/App.jsx
+++ src/main/webui/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     return (
         <>
             <h1 className="title">The Pet Ai-doption Ai-gency</h1>
+            <h2 className="featured-pet-heading">Today's Featured Pet</h2>
             <div className="invitation">This is a wonderful ai-pportunity for you to ai-dopt:</div>
             <Pet></Pet>
 
